69f5aef721d3a79cc2c60154ac1778541aa2dc3a
du929 <877940547@qq.com>
2021-04-20 10:40:11 +0800

    '楼楼商城api项目初始化'

---
+++ src/test/java/xyz/zfdu/mall/api/MallApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.zfdu.mall.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
